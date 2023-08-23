6f4379d43ab51634f35a00903936a927776fcf94
Sloly <yahoo3511@gmail.com>
2023-08-23 20:45:31 +0800

    first commit

---
+++ src/api/shopitem/routes/shopitem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shopitem router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shopitem.shopitem');
---
+++ src/api/shopitem/services/shopitem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shopitem service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::shopitem.shopitem');
